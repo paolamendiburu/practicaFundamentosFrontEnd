dac12726d01e19a7bc3c52ccfff41ad3e52b4d32
Paola Mendiburu <paolamendiburualetti@gmail.com>
2018-04-02 23:09:39 +0200

    arreglo ajax.js

--- scripts/ajax2.js
+++ scripts/ajax2.js
@@ -1,29 +1,29 @@
-var form = document.getElementsByName("contacto")[0];
 var xhr = new XMLHttpRequest();
 
-function getData() {
-  makeRequest("GET", "http://localhost:8000/api/prueba", null, function (data) {
-    var response = JSON.parse(data);
+var form = document.getElementsByName("contacto")[0];
 
+function getData() {
+  makeRequest("GET", "http://localhost:8000/api/task", null, function (data) {
+    var response = JSON.parse(data);
     var div = document.createElement("div");
     var children = "";
     response.forEach(element => {
       children += "<p>" + element.name + "</p>";
     });
+
     div.innerHTML = children;
+
     form.appendChild(div);
   });
-
 }
 
 function createData() {
   var data = {
-    name: 'Paola'
+    name: "Prueba"
   };
-  makeRequest("POST", "http://localhost:8000/api/prueba", data, function () {
+  makeRequest("POST", "http://localhost:8000/api/task", data, function () {
     console.log("Datos creados correctamente");
   });
-
 }
 //createData();
 getData();
@@ -46,6 +46,3 @@ function makeRequest(method, url, body, callbackSuccess) {
     xhr.send();
   }
 }
-
-
-
